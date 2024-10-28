d761a3c705fd7dcded66dc974bfd6c8f2afb11fd
Puneeth Venkat <puneethvenkat2k03@gmail.com>
2024-10-28 20:41:37 +0530

    created a basic header

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",              // The main HTML file in the root of the project
+    "./src/**/*.{js,jsx,ts,tsx}", // All JS, JSX, TS, and TSX files in `src` folder and subfolders
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
