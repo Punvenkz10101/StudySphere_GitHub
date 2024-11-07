e3ac0383a3061ee115df5e6b185fa7e3397f553b
Harshakumarsm <harshakumarsm@gmail.com>
2024-11-07 22:23:58 +0530

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,6 @@
 // App.js
 import React from 'react';
 import './app.css';
-<<<<<<< HEAD
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import SignInPage from './components/SignInPage'; 
-import SignUpPage from './components/SignUpPage'; // Ensure path is correct
-
-function App() {
-  return (
-   <>
-      <Router>
-      <Routes>
-        {/* Default route to redirect from "/" to "/signin" */}
-        <Route path="/" element={<Navigate to="/signin" />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-      </Routes>
-    </Router>
-   </>
-     
-=======
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import Features from './components/Features';
@@ -30,8 +11,8 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className='h-screen w-screen overflow-x-hidden'>
-      <Header /> {/* Header moved outside HeroSection */}
+    <div className=' overflow-x-hidden'>
+      <Header /> {/* Header moved outside HeroSection */} //fixed
 
       <section id="hero">
         <HeroSection />
@@ -60,7 +41,6 @@ function App() {
 
       <Footer />
     </div>
->>>>>>> 552686cd1657d070e9b8d8027d1af7a7a78392cb
   );
 }
 
