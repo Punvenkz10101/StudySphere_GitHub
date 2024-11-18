a6e220e1749af66060427be06f29e92e95f826b4
Puneeth Venkat <puneethvenkat2k03@gmail.com>
2024-11-18 13:29:46 +0530

    Server.js changes

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // Database Connection
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected successfully!"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
