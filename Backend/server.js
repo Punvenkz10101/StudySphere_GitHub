e748aa8bd9e75d04ad30dac4cc601609fe95b78b
Bharath Pothula <Bharathkkl907@gmail.com>
2024-11-25 18:54:07 +0530

    modifications done in .env and server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,31 +1,32 @@
-// backend/server.js
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const roomRoutes = require("./routes/roomRoutes");
+const contactRoutes = require('./routes/contactRoutes');
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
 
 try {
-  // Database Connection
+ 
   mongoose
     .connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+
     })
     .then(() => console.log("MongoDB connected successfully!"))
     .catch((err) => console.error("MongoDB connection error:", err));
 } catch (error) {
   console.error("Error connecting to MongoDB:", error);
 }
-// Routes
-app.use("/api/rooms", roomRoutes);
+
+app.use("/api/rooms", (req, res, next) => { console.log("/api/rooms route accessed"); next(); }, roomRoutes); 
+app.use('/api/contacts', (req, res, next) => { console.log("/api/contacts route accessed"); next(); }, contactRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
