0e61109c172bf8c136d9310c1bc49665ae2b0cae
Puneeth Venkat <puneethvenkat2k03@gmail.com>
2024-12-01 12:26:24 +0530

    Testing 5

--- server/index.js
+++ server/index.js
@@ -1,3 +1,11 @@
+const cors = require('cors');
+
+app.use(cors({
+    origin: 'https://study-sphere-git-hub.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
 app.use((req, res, next) => {
     const origin = req.headers.origin;
     
