114ea945f3e8f7d7a4d9e9b043b9de510c9252ad
Puneeth Venkat <puneethvenkat2k03@gmail.com>
2024-12-03 22:08:59 +0530

    minor edit

--- Frontend/src/services/socketService.js
+++ Frontend/src/services/socketService.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 const SOCKET_URL = process.env.NODE_ENV === 'production' 
   ? process.env.VITE_SOCKET_URL 
-  : 'http://localhost:5001';
+  : 'https://studysphere-github.onrender.com';
 
 const socket = io(SOCKET_URL, {
   transports: ['websocket', 'polling'],
